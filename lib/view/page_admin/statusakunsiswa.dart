348ab82f0bb8a38e72c156109bb8fca4a9dd2803
arhan321 <arhanmcz@gmail.com>
2025-05-02 00:07:52 +0700

    fetch data siswa yang tidak hadir lebih dari 3x dan direct ke WA, tapi belum work

--- lib/view/page_admin/statusakunsiswa.dart
+++ lib/view/page_admin/statusakunsiswa.dart
@@ -1,29 +1,57 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
 
-class StatusAkunSiswa extends StatelessWidget {
-  final List<Map<String, String>> akunSiswa = [
-    {
-      'nama': 'Ahmad Faiz',
-      'email': 'ahmadfaiz@email.com',
-      'kelas': 'X IPA 1',
-      'ttl': 'Bandung, 12 Jan 2007',
-      'absen': '10',
-      'noOrtu': '6289652731947',
-      'tidakHadir': '3',
-    },
-    {
-      'nama': 'Bunga Lestari',
-      'email': 'bunga@email.com',
-      'kelas': 'XI IPS 2',
-      'ttl': 'Jakarta, 5 Mei 2006',
-      'absen': '07',
-      'noOrtu': '6282112125639',
-      'tidakHadir': '2',
-    },
-  ];
+class StatusAkunSiswa extends StatefulWidget {
+  const StatusAkunSiswa({Key? key}) : super(key: key);
+
+  @override
+  State<StatusAkunSiswa> createState() => _StatusAkunSiswaPageState();
+}
+
+class _StatusAkunSiswaPageState extends State<StatusAkunSiswa> {
+  bool isLoading = true;
+  List<Map<String, dynamic>> siswaList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchSiswaData();
+  }
+
+  Future<void> fetchSiswaData() async {
+    final url = Uri.parse("https://absen.randijourney.my.id/api/v1/account");
+
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+
+        final List<Map<String, dynamic>> filteredData = data
+            .where((item) =>
+                item is Map<String, dynamic> &&
+                item['role'] != null &&
+                item['role'].toString().toLowerCase() == 'siswa')
+            .cast<Map<String, dynamic>>()
+            .toList();
+
+        setState(() {
+          siswaList = filteredData;
+          isLoading = false;
+        });
+      } else {
+        throw Exception("Gagal memuat data");
+      }
+    } catch (e) {
+      debugPrint("ERROR: $e");
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,77 +76,86 @@ class StatusAkunSiswa extends StatelessWidget {
             end: Alignment.bottomRight,
           ),
         ),
-        child: ListView.builder(
-          itemCount: akunSiswa.length,
-          itemBuilder: (context, index) {
-            final siswa = akunSiswa[index];
-            return Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)),
-              elevation: 4,
-              margin: EdgeInsets.only(bottom: 16),
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(Icons.person, size: 50, color: Colors.grey),
-                    SizedBox(width: 12),
-                    Expanded(
-                      child: Column(
+        child: isLoading
+            ? Center(child: CircularProgressIndicator(color: Colors.white))
+            : ListView.builder(
+                itemCount: siswaList.length,
+                itemBuilder: (context, index) {
+                  final siswa = siswaList[index];
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    elevation: 4,
+                    margin: EdgeInsets.only(bottom: 16),
+                    child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            siswa['nama'] ?? '',
-                            style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              color: Colors.deepPurple,
-                            ),
-                          ),
-                          Text(siswa['email'] ?? ''),
-                          SizedBox(height: 4),
-                          Text('Kelas: ${siswa['kelas']}'),
-                          Text('TTL: ${siswa['ttl']}'),
-                          Text('No. Absen: ${siswa['absen']}'),
-                          Text('No. HP Ortu: ${siswa['noOrtu']}'),
-                          SizedBox(height: 6),
-                          Text(
-                            'Tidak hadir: ${siswa['tidakHadir']}',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontStyle: FontStyle.italic,
-                              color: Colors.red,
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          InkWell(
-                            onTap: () async {
-                              final noHp = siswa['noOrtu']!;
-                              final nama = siswa['nama']!;
-                              final url =
-                                  'https://wa.me/$noHp?text=Halo%20Orang%20Tua%20dari%20$nama%2C%20kami%20ingin%20menyampaikan%20informasi%20kehadiran.';
-
-                              if (await canLaunchUrl(Uri.parse(url))) {
-                                await launchUrl(Uri.parse(url),
-                                    mode: LaunchMode.externalApplication);
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                      content: Text('Tidak bisa membuka WhatsApp')),
-                                );
-                              }
-                            },
-                            child: Row(
+                          Icon(Icons.person, size: 50, color: Colors.grey),
+                          SizedBox(width: 12),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                               FaIcon(FontAwesomeIcons.whatsapp, color: Colors.green),
-
-                                SizedBox(width: 8),
                                 Text(
-                                  'Hubungi Orang Tua via WhatsApp',
+                                  siswa['name'] ?? '-',
+                                  style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                    color: Colors.deepPurple,
+                                  ),
+                                ),
+                                Text(siswa['email'] ?? '-'),
+                                SizedBox(height: 4),
+                                Text('Kelas: ${siswa['kelas'] ?? '-'}'),
+                                Text('TTL: ${siswa['tanggal_lahir'] ?? '-'}'),
+                                Text(
+                                    'No. Absen: ${siswa['nomor_absen'] ?? '-'}'),
+                                Text(
+                                    'No. HP Ortu: ${siswa['nomor_telfon'] ?? '-'}'),
+                                SizedBox(height: 6),
+                                Text(
+                                  'Tidak hadir: ${siswa['tidak_hadir'] ?? '0'}',
                                   style: TextStyle(
-                                    color: Colors.green[800],
-                                    fontWeight: FontWeight.w600,
+                                    fontWeight: FontWeight.bold,
+                                    fontStyle: FontStyle.italic,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                InkWell(
+                                  onTap: () async {
+                                    final noHp = siswa['nomor_telfon'] ?? '';
+                                    final nama = siswa['name'] ?? '';
+                                    final url =
+                                        'https://wa.me/$noHp?text=Halo%20Orang%20Tua%20dari%20$nama%2C%20kami%20ingin%20menyampaikan%20informasi%20kehadiran.';
+
+                                    if (await canLaunchUrl(Uri.parse(url))) {
+                                      await launchUrl(Uri.parse(url),
+                                          mode: LaunchMode.externalApplication);
+                                    } else {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(
+                                        SnackBar(
+                                            content: Text(
+                                                'Tidak bisa membuka WhatsApp')),
+                                      );
+                                    }
+                                  },
+                                  child: Row(
+                                    children: [
+                                      FaIcon(FontAwesomeIcons.whatsapp,
+                                          color: Colors.green),
+                                      SizedBox(width: 8),
+                                      Text(
+                                        'Hubungi Orang Tua via WhatsApp',
+                                        style: TextStyle(
+                                          color: Colors.green[800],
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ],
@@ -127,12 +164,9 @@ class StatusAkunSiswa extends StatelessWidget {
                         ],
                       ),
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
-            );
-          },
-        ),
       ),
     );
   }
